88635ce22bf5b5508558652ecd54474f11b8a3bf
Sally E <sme418@nyu.edu>
2022-10-04 09:06:53 -0400

    add packages and clean out

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { WEATHER_APP_API_KEY } from "../API_KEYS";
 
 function Home(){
     return (<div>
         <h1>Weather App</h1>
+        <p>{WEATHER_APP_API_KEY}</p>
     </div>)
 }
 
